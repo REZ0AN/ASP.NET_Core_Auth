781a64f91e92033b88e417b9218850e376ae5b3d
REZ-OAN <ahmedabir.rez@gmail.com>
2025-02-13 14:10:56 +0600

    updated code structure
    - separated AuthService which will handle creation of auth encrypt data
    - a middleware which will recognize, decrypt data pass the info to the controllers

--- auth_service/Program.cs
+++ auth_service/Program.cs
@@ -1,12 +1,21 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.DataProtection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDataProtection();
 
+builder.Services.AddHttpContextAccessor();
+// authentication service - for creating and setting the cookie
+builder.Services.AddScoped<AuthService>();
+
 var app = builder.Build();
-app.MapGet("/username", (HttpContext ctx, IDataProtectionProvider idp) => {
-    
+
+
+// authentication middleware - for recognizing
+app.Use((ctx, next)=> {
+
+    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
     var protector = idp.CreateProtector("auth-cookie");
     var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
     
@@ -16,15 +25,48 @@ app.MapGet("/username", (HttpContext ctx, IDataProtectionProvider idp) => {
     var key = username[0];
     var value = username[1];
 
-    return value;
+    var claims = new List<Claim>();
+    claims.Add(new Claim(key, value));
+    var identity = new ClaimsIdentity(claims);
+
+    ctx.User = new ClaimsPrincipal(identity);
+
+    return next();
 });
 
-app.MapGet("/login", (HttpContext ctx, IDataProtectionProvider idp) => {
 
-    var protector = idp.CreateProtector("auth-cookie");
-    ctx.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:abir")}";
+app.MapGet("/username", (HttpContext ctx) => {
 
+    return ctx.User.FindFirstValue("usr");
+});
+
+app.MapGet("/login", (AuthService auth) => {
+
+    auth.SignIn("abir");
     return "ok";
 });
 
 app.Run();
+
+
+public class AuthService {
+
+    // to have the IDataProtectionProvider
+    
+    private readonly IDataProtectionProvider _idp;
+
+    // to access the HttpContext
+    private readonly IHttpContextAccessor _accessor;
+    
+    public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor) {
+        _idp = idp;
+        _accessor = accessor;
+    }
+
+    public void SignIn(string username){
+        var protector = _idp.CreateProtector("auth-cookie");
+        _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:"+username)}";
+    }
+
+
+}
